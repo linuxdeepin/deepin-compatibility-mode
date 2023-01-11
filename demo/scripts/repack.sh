a9ab6271e77774449c2769df4baf9fa0b0ea56ef
black-desk <me@black-desk.cn>
2023-01-11 09:48:14 +0800

    Fix some shellcheck warnings

--- demo/scripts/repack.sh
+++ demo/scripts/repack.sh
@@ -3,45 +3,45 @@
 set -x
 
 function handle_usr_desktop() {
-        desktops=$(find "$1"/applications -name "*.desktop")
+	desktops=$(find "$1"/applications -name "*.desktop")
 
-        for desktop in "${desktops[@]}"; do
-                cp "$desktop" $2
-                sed "s/Exec=/Exec=loader --deb-id $(basename "$file") -- /g" -i $2/"$(basename "$desktop")"
-        done
+	for desktop in "${desktops[@]}"; do
+		cp "$desktop" "$2"
+		sed "s/Exec=/Exec=loader --deb-id $(basename "$file") -- /g" -i "$2"/"$(basename "$desktop")"
+	done
 }
 
 file=$1
-file_real_path="$(readlink -f $file)"
+file_real_path="$(readlink -f "$file")"
 
 rm -rf .tmp
 mkdir .tmp
 
 pushd .tmp || return
 
-dpkg-deb -R $file_real_path tmp
+dpkg-deb -R "$file_real_path" tmp
 
-filename=$(basename $file)
+filename=$(basename "$file")
 array=(${filename//_/ })
 working_dir=working_dir/opt/apps/${array[0]}/entries/
-mkdir -p $working_dir
-mkdir -p $working_dir/applications/
+mkdir -p "$working_dir"
+mkdir -p "$working_dir"/applications/
 
 if [[ -d "tmp/usr" ]]; then
-    handle_usr_desktop tmp/usr/share/ $working_dir/applications/
+	handle_usr_desktop tmp/usr/share/ "$working_dir"/applications/
 fi
 
 if [[ -d "tmp/opt" ]]; then
-    apps=$(ls tmp/opt/apps)
-        for p in "${apps[@]}"; do
-                handle_usr_desktop tmp/opt/apps/$p/entries $working_dir/applications/
-        cp -a tmp/opt/apps/$p/entries/icons $working_dir/ # check exists
-        cp -a tmp/opt/apps/$p/info $working_dir
-        done
+	apps=$(ls tmp/opt/apps)
+	for p in "${apps[@]}"; do
+		handle_usr_desktop tmp/opt/apps/"$p"/entries "$working_dir"/applications/
+		cp -a tmp/opt/apps/"$p"/entries/icons "$working_dir"/ # check exists
+		cp -a tmp/opt/apps/"$p"/info "$working_dir"
+	done
 fi
 
-mkdir -p $working_dir/files
-cp $file_real_path $working_dir/files/
+mkdir -p "$working_dir"/files
+cp "$file_real_path" "$working_dir"/files/
 
 sed /Depends:/d -i tmp/DEBIAN/control
 sed /Recommends:/d -i tmp/DEBIAN/control
